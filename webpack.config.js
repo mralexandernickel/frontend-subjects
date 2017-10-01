33868c98fd550ce1abdf690f75d4c502b945c6ee
Alexander Nickel <mr.alexander.nickel@gmail.com>
2017-10-01 12:33:56 +0200

    Configure webpack to export all modules inside global namespace

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 module.exports = {
   entry: {
-      module: './index.ts'
+    module: './index.ts'
   },
   output: {
-      filename: './dist/frontend.subjects.js',
-      libraryTarget: 'umd',
-      umdNamedDefine: true
+    filename: './dist/frontend.subjects.js',
+    library: 'FrontendSubjects',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   resolve: {
-      // Add '.ts' and '.tsx' as a resolvable extension.
-      extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    // Add '.ts' and '.tsx' as a resolvable extension.
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
-      loaders: [
-          // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-          {
-            test: /\.tsx?$/,
-            loader: 'ts-loader'
-          }
-      ]
+    loaders: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
   }
 }
