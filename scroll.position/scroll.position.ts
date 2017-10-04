c7398714ceff5ed687efca7c44ce2cfdbf781a51
Alexander Nickel <mr.alexander.nickel@gmail.com>
2017-10-04 22:02:32 +0200

    Add ScrollPositionSubject to exported members

--- scroll.position/scroll.position.ts
+++ scroll.position/scroll.position.ts
@@ -4,6 +4,23 @@ import ScrollSubject from '../event/scroll.subject';
 /**
  * ScrollPositionSubject
  * =====================
+ *
+ * ## Usage
+ *
+ * ```typescript
+ * import {
+ *   ScrollPositionSubject
+ * } from '@mralexandernickel/frontend-subjects/scroll.position/scroll.position';
+ *
+ * export default new ScrollPositionSubject();
+ * ```
+ *
+ * ```typescript
+ * import ScrollPosition from './ScrollPosition';
+ * ScrollPosition.get().subscribe(scrollPosition => {
+ *   console.log('ScrollPosition is now:', scrollPosition);
+ * });
+ * ```
  */
 export class ScrollPositionSubject {
 
