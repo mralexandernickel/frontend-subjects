ffb91c146e2ee8b4c996c3f2afeb57cdfb955389
Alexander Nickel <mr.alexander.nickel@gmail.com>
2017-12-08 12:05:24 +0100

    Fix wrong ConnectionStatus

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export * from './screensize.detection/screensize.detection';
 export * from './scroll.direction/scroll.direction';
 export * from './scroll.position/scroll.position';
 export * from './connection.speed/connection.speed';
+export * from './connection.status/connection.status';
